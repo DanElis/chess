09bdb0246f8ddb26dc32b4b969284587f840ae6a
wer <https://werr@bitbucket.org/werr/nsu.git>
2017-12-14 13:23:50 +0700

    cmake

---
+++ cmake-build-debug/CMakeFiles/chess.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wer/CLionProjects/chess/bishop.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/bishop.cpp.o"
+  "/home/wer/CLionProjects/chess/chessboard.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/chessboard.cpp.o"
+  "/home/wer/CLionProjects/chess/king.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/king.cpp.o"
+  "/home/wer/CLionProjects/chess/knight.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/knight.cpp.o"
+  "/home/wer/CLionProjects/chess/main.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/main.cpp.o"
+  "/home/wer/CLionProjects/chess/pawn.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/pawn.cpp.o"
+  "/home/wer/CLionProjects/chess/queen.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/queen.cpp.o"
+  "/home/wer/CLionProjects/chess/rook.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/rook.cpp.o"
+  "/home/wer/CLionProjects/chess/space.cpp" "/home/wer/CLionProjects/chess/cmake-build-debug/CMakeFiles/chess.dir/space.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/chess.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess.dir/main.cpp.o"
+  "CMakeFiles/chess.dir/king.cpp.o"
+  "CMakeFiles/chess.dir/queen.cpp.o"
+  "CMakeFiles/chess.dir/knight.cpp.o"
+  "CMakeFiles/chess.dir/pawn.cpp.o"
+  "CMakeFiles/chess.dir/bishop.cpp.o"
+  "CMakeFiles/chess.dir/rook.cpp.o"
+  "CMakeFiles/chess.dir/space.cpp.o"
+  "CMakeFiles/chess.dir/chessboard.cpp.o"
+  "chess.pdb"
+  "chess"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
